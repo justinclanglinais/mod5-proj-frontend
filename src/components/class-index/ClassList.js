39effca541c3516bcd0c73d39478354bb190c838
Justin Langlinais <justinclanglinais@gmail.com>
2020-07-20 15:13:05 -0700

    troubleshooting nested routes

--- src/components/class-index/ClassList.js
+++ src/components/class-index/ClassList.js
@@ -4,17 +4,15 @@ import ClassCard from '../ClassCard.js'
 
 const ClassList = ({sessions}) => {
     const renderClasses = sessions.map(s => 
-        <div><Link key={s.id} to={`/classes/${s.id}`}><ClassCard thisSession={s}/></Link></div> 
+        // <div><Link key={s.id} to={`/classes/${s.id}`}><ClassCard thisSession={s}/></Link></div> 
+        <div><Link key={s.id} to={`/classes/${s.id}`}><h1>{s.topic.name}</h1></Link></div> 
     )
     return (
         <div>
             <div>
-                <h2>`Showing (NUM) classes for (DATE) for (CATEGORY)`</h2>
+                <h2>{`Showing (NUM) classes for (DATE) for (CATEGORY)`}</h2>
             </div>
             <div>
-                {/* {this.state.sessions.map(session=>{
-                    return <ClassCard thisSession={session}/>
-                })} */}
                 {renderClasses}
             </div>
         </div>
