7b611a335194c07b919dd54829121787c6d7ab14
Justin Langlinais <justinclanglinais@gmail.com>
2020-07-20 17:08:04 -0700

    adjusted index position for props in class show

--- src/components/class-show/ClassShow.js
+++ src/components/class-show/ClassShow.js
@@ -6,12 +6,13 @@ import Topic from './Topic.js'
 const ClassShow = ({match,sessions}) => {
     {console.log(match,"inside Class Show")}
     {console.log(sessions[match.params.id], "inside class Show")}
-    const { time , user , category , topic } = sessions[match.params.id]
+    const { id, time , user , category , topic } = sessions[match.params.id - 1]
     return (
         <div>
             <div>
                 <h1>Class Show</h1>
-                <h2>{category.name}</h2>
+                <h2>{category.name} - {time}</h2>
+                <h1>{id}</h1>
             </div>
             <div>
                 <Topic topic={topic}/>
