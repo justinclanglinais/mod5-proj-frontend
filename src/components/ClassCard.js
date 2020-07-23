db0c42cbd5a92c482f60ff53aceb984db7c52a38
Justin Langlinais <justinclanglinais@gmail.com>
2020-07-22 17:54:09 -0700

    fixed classcard ids and nested routing

--- src/components/ClassCard.js
+++ src/components/ClassCard.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+
+import {Card} from 'react-bootstrap'
 
 export default class ClassCard extends Component {
     render() {
         const { id, time, topic, category, user } = this.props.thisSession
         return (
             <div>
-                <div>
+                <Card style={{ width: '18rem' }} align='center'>
+                    <Card.Body>
+                        <Card.Title>{topic.name}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">{category.name}</Card.Subtitle>
+                        <Card.Text>{time}</Card.Text>
+                        <Link to={`/classes/${id}`}>View Details</Link>
+                        <Card.Link href="#">Sign Up</Card.Link>
+                    </Card.Body>
+                </Card>
+                {/* <div>
                     <h3>{time}</h3>
-                    {/* <h3>{id}</h3> */}
+                    <h3>{id}</h3>
                     <h3>Duration: {category.name.includes("Kids") ? "45 minutes" : "60 minutes"}</h3>
                     <h2>{topic.name}</h2>
                     <h3>{category.name}</h3>
@@ -15,8 +27,8 @@ export default class ClassCard extends Component {
                 </div>
                 <div>
                     <button>View Details</button>
-                    {/* <button>Sign Up</button> */}
-                </div>
+                    <button>Sign Up</button>
+                </div> */}
             </div>
         )
     }
--- src/components/class-index/ClassList.js
+++ src/components/class-index/ClassList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 import ClassCard from '../ClassCard.js'
+import { Container } from 'react-bootstrap'
 
 const ClassList = ({sessions}) => {
     const renderClasses = sessions.map(s => 
-        <div><Link key={s.id} to={`/classes/${s.id}`}><ClassCard thisSession={s}/></Link></div> 
-        // <div><Link key={s.id} to={`/classes/${s.id}`}><h1>{s.topic.name}</h1></Link></div> 
+        // <div><Link key={s.id} to={`/classes/${s.id}`}><ClassCard thisSession={s}/></Link></div>
+        <ClassCard thisSession={s}/>
     )
     return (
         <div>
