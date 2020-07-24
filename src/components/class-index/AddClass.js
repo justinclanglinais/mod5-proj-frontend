43b4c5cd1a5ede129cb8c91cfbd9fbc984b74676
Justin Langlinais <justinclanglinais@gmail.com>
2020-07-24 11:30:33 -0700

    add class component built

--- src/components/class-index/AddClass.js
+++ src/components/class-index/AddClass.js
@@ -1,14 +1,34 @@
 import React from 'react'
+import DateTimePicker from 'react-datetime-picker'
 
 export default class AddClass extends React.Component {
     state = {
 
     }
+    // handleDateChange = (e) => {
+    //     // console.log(e)
+    //     this.setState({
+    //         dateTime: e.target.value
+    //     })
+    //     console.log(this.state.dateTime)
+    // }
+    handleChange = (e) => {
+        this.setState({
+            ...this.state, [e.target.name] : e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+    }
     render () {
         const {users, topics, categories} = this.props
         return (
             <div>
-                <form>
+                <form onChange={this.handleChange} onSubmit={this.handleSubmit}>
+                    {/* <div>
+                        <DateTimePicker onChange={this.handleDateChange}/>
+                    </div> */}
                     <div>
                         <label>Topic</label>
                         <select name="topic_id">
