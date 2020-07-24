2ed31aec7f28d894f856e15c6a28cf8320918010
Justin Langlinais <justinclanglinais@gmail.com>
2020-07-24 12:19:08 -0700

    building redux

--- src/components/class-index/AddClass.js
+++ src/components/class-index/AddClass.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import DateTimePicker from 'react-datetime-picker'
+import { connect } from 'react-redux';
 
-export default class AddClass extends React.Component {
+class AddClass extends React.Component {
     state = {
-
+        topic_id : 1,
+        category_id : 1,
+        user_id : 1,
     }
     // handleDateChange = (e) => {
     //     // console.log(e)
@@ -19,7 +22,7 @@ export default class AddClass extends React.Component {
     }
     handleSubmit = (e) => {
         e.preventDefault()
-        console.log(this.state)
+        this.props.addClass(this.state)
     }
     render () {
         const {users, topics, categories} = this.props
@@ -31,7 +34,7 @@ export default class AddClass extends React.Component {
                     </div> */}
                     <div>
                         <label>Topic</label>
-                        <select name="topic_id" >
+                        <select name="topic_id" value={this.state.topic_id}>
                             {topics.map(topic=> {
                                 return <option value={topic.id}>{topic.name}</option>
                             })}
@@ -39,7 +42,7 @@ export default class AddClass extends React.Component {
                     </div>
                     <div>
                         <label>Category</label>
-                        <select name="category_id">
+                        <select name="category_id" value={this.state.category_id}>
                             {categories.map(category=> {
                                 return <option value={category.id}>{category.name}</option>
                             })}
@@ -47,7 +50,7 @@ export default class AddClass extends React.Component {
                     </div>
                     <div>
                         <label>Instructor</label>
-                        <select name="user_id">
+                        <select name="user_id" value={this.state.user_id}>
                             {users.map(user=> {
                                 return <option value={user.id}>{user.name}</option>
                             })}
@@ -57,7 +60,17 @@ export default class AddClass extends React.Component {
                         <button>Add Class</button>
                     </div>
                 </form>
+                {this.state.topic_id}
+                {this.state.category_id}
+                {this.state.user_id}
+                
             </div>
         )
     }
 }
+const mapDispatchToProps = dispatch => {
+    return {
+      addClass: formData => dispatch({type: 'ADD_CLASS', payload: formData})
+    }
+  }
+export default connect(null, mapDispatchToProps)(AddClass)
--- src/reducers/sessions.js
+++ src/reducers/sessions.js
@@ -1,9 +1,14 @@
-export default (state = [], action) => {
-    let idx
+export default function manageSessions(state = {
+    sessions: [],
+  }, action) {
     switch (action.type) {
-        case 'ADD_SESSION':
-            return [...state , action.session]
-        default:
-            return state
+      case 'ADD_CLASS':
+   
+        console.log({ sessions: state.sessions.concat(action.payload) });
+   
+        return { sessions: state.sessions.concat(action.payload) };
+   
+      default:
+        return state;
     }
-}
+  }
