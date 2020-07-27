8d4d7f49a742e277e525604e1e46ba1e5dc115e2
Justin Langlinais <justinclanglinais@gmail.com>
2020-07-27 16:03:11 -0700

    slight CSS adjustment on class card

--- src/components/ClassCard.js
+++ src/components/ClassCard.js
@@ -9,22 +9,18 @@ export default class ClassCard extends Component {
     render() {
         const { id, time, topic, category, user } = this.props.thisSession
         return (
-            <div>
-                <div className="ClassCard">
-                    <Card  style={{ width: '18rem' }} align='center'>
-                        {/* <div className="container-row"> */}
-                            <div className="card-body">
-                                {time} - {category.name.includes("Kids") ? "(45 min)" : "(60 min)"}
-                                <h3>{topic.name}</h3>
-                                <Card.Title>{category.name}</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">Instructor: {user.name}</Card.Subtitle>
-                            </div>
-                            <div className="card-buttons">
-                            <Link className="card-button-2" to={`/classes/${id}`}>View Details</Link>
-                                <button className="card-button" onClick={this.handleSignUpSession}>Sign Up</button>
-                            </div>
-                        {/* </div> */}
-                    </Card>
+            <div className="ClassCard">
+                <div className="container-row">
+                    <div className="card-body">
+                        {time} - {category.name.includes("Kids") ? "(45 min)" : "(60 min)"}
+                        <h3>{topic.name}</h3>
+                        <Card.Title>{category.name}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Instructor: {user.name}</Card.Subtitle>
+                    </div>
+                    <div className="card-buttons">
+                        <Link className="card-button-2" to={`/classes/${id}`}>View Details</Link>
+                        <button className="card-button" onClick={this.handleSignUpSession}>Sign Up</button>
+                    </div>
                 </div>
             </div>
         )
