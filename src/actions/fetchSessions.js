f63f3f514b82771cd80df9054abeeb04a07e129f
Justin Langlinais <justinclanglinais@gmail.com>
2020-07-28 10:17:15 -0700

    formatting

--- src/actions/fetchSessions.js
+++ src/actions/fetchSessions.js
@@ -2,10 +2,10 @@ import { Api } from '../services/Api.js'
 
 
 export function fetchSessions() {
-    return (dispatch) => {
-      dispatch({ type: 'START_ADDING_SESSIONS_REQUEST' });
-      fetch(`${Api.API_ROOT}/sessions`)
-        .then(response => response.json())
-        .then(sessions => dispatch({ type: 'ADD_SESSIONS', sessions }));
-    };
-  }
+  return (dispatch) => {
+    dispatch({ type: 'START_ADDING_SESSIONS_REQUEST' });
+    fetch(`${Api.API_ROOT}/sessions`)
+      .then(response => response.json())
+      .then(sessions => dispatch({ type: 'ADD_SESSIONS', sessions }));
+  };
+}
