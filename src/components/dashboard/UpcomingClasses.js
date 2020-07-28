19e7a2f88e8e2d0ddade0c5aa63a70243b880a6a
Justin Langlinais <justinclanglinais@gmail.com>
2020-07-27 17:52:14 -0700

    clean up upcoming class rendering with space between cards

--- src/components/dashboard/UpcomingClasses.js
+++ src/components/dashboard/UpcomingClasses.js
@@ -5,9 +5,12 @@ import { Container, Row, Col } from 'react-bootstrap'
 export default class UpcomingClasses extends Component {
 
     render() {
-        const renderClasses = this.props.sessions && this.props.sessions.map(s=>{
-            return <ClassCard key={s.id} thisSession={s}/>
-        })
+        const renderClasses = this.props.sessions && this.props.sessions.map(s=>
+            <div>
+                <ClassCard key={s.id} thisSession={s}/>
+                <p></p>
+            </div>
+        )
         return (
             <div>
                 <div>
