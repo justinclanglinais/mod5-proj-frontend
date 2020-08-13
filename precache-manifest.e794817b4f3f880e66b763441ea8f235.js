01f907ccbc77113c5392290a00e07360b11cd959
Justin Langlinais <justinclanglinais@gmail.com>
2020-08-13 13:01:31 -0700

    Updates

---
+++ precache-manifest.e794817b4f3f880e66b763441ea8f235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7152818b4d7e1613fe84c79e85cc0060",
+    "url": "/mod5-proj-frontend/index.html"
+  },
+  {
+    "revision": "c5c67ae26e02b42bc1b2",
+    "url": "/mod5-proj-frontend/static/css/main.197d3c26.chunk.css"
+  },
+  {
+    "revision": "764635856d8898ad1517",
+    "url": "/mod5-proj-frontend/static/js/2.b36dfdcb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mod5-proj-frontend/static/js/2.b36dfdcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5c67ae26e02b42bc1b2",
+    "url": "/mod5-proj-frontend/static/js/main.3c8fce7e.chunk.js"
+  },
+  {
+    "revision": "b64b145927840e8579e0",
+    "url": "/mod5-proj-frontend/static/js/runtime-main.f2ca423f.js"
+  }
+]);
