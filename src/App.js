839c4f5bbf7e13c82f9c7fe885fa84cb648bc63a
Justin Langlinais <justinclanglinais@gmail.com>
2020-10-28 21:53:18 -0700

    cleanup code

--- src/App.js
+++ src/App.js
@@ -162,20 +162,69 @@ class App extends React.Component {
       <div className="App">
         <Router>
           <div>
-            <Navbar loggedIn={this.state.loggedIn}/>
-            {this.state.loggedIn ? <button onClick={this.handleLogout}>LOG OUT</button> : null}
-            <Route exact path="/" render={() => <h1>Jiu-Jitsu Class Management Home Page</h1>} />
-            {this.state.loggedIn ? <Route exact path="/dashboard" render={() => <Dashboard user={this.state.auth.user.user} deleteEnrollment={this.deleteEnrollment} />} /> : null }
-            <Route exact path="/signup" 
-              render={routerProps => <Signup {...routerProps} 
-              handleLogin={this.handleLogin} />} 
+            <Navbar 
+              loggedIn={this.state.loggedIn}
             />
-            <Route exact path="/login" 
-              render={props => { return <Login {...props}
-              handleLogin={this.handleLogin} /> } }
+            {this.state.loggedIn ? 
+              <button onClick={this.handleLogout}>LOG OUT</button> :
+              null}
+            <Route
+              exact path="/" 
+              render={() => <h1>Jiu-Jitsu Class Management Home Page</h1>} 
             />
-            {this.state.loggedIn ? <Route exact path="/classes" render={routerProps => <ClassIndex {...routerProps} sessions={this.state.sessions} users={this.state.users} topics={this.state.topics} categories={this.state.categories} addSession={this.addSession} signUpSession={this.signUpSession} user={this.state.auth.user.user} />} /> : null }
-            <Route path={`/classes/:id`} render={routerProps => <ClassShow {...routerProps} sessions={this.state.sessions} sendEdit={this.sendEdit} users={this.state.users} topics={this.state.topics} categories={this.state.categories} signUpSession={this.signUpSession} deleteSession={this.deleteSession} />} />
+            {this.state.loggedIn ? 
+              <Route exact path="/dashboard" 
+                render={() => 
+                  <Dashboard user={this.state.auth.user.user} deleteEnrollment={this.deleteEnrollment} />}
+              /> :
+              null}
+            <Route 
+              exact path="/signup" 
+              render={routerProps => 
+                <Signup 
+                  {...routerProps} 
+                  handleLogin={this.handleLogin} 
+                />} 
+            />
+            <Route 
+              exact path="/login" 
+              render={props => { return <Login 
+                    {...props}
+                    handleLogin={this.handleLogin} 
+                  /> 
+                } 
+              }
+            />
+            {this.state.loggedIn ?
+              <Route 
+                exact path="/classes" 
+                render={routerProps => 
+                  <ClassIndex 
+                    {...routerProps} 
+                    sessions={this.state.sessions} 
+                    users={this.state.users} 
+                    topics={this.state.topics} 
+                    categories={this.state.categories} 
+                    addSession={this.addSession} 
+                    signUpSession={this.signUpSession} 
+                    user={this.state.auth.user.user} 
+                  />} 
+              /> :
+              null}
+            <Route 
+              path={`/classes/:id`} 
+              render={routerProps => 
+                <ClassShow {...routerProps} 
+                  sessions={this.state.sessions} 
+                  sendEdit={this.sendEdit} 
+                  users={this.state.users} 
+                  topics={this.state.topics} 
+                  categories={this.state.categories} 
+                  signUpSession={this.signUpSession} 
+                  deleteSession={this.deleteSession} 
+                />
+              } 
+            />
           </div>
         </Router>
       </div>
